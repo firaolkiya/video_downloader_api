b9323a1e0a88993b4912e1e732c6a9f0efc904e4
firaolkiya <firaolkiya6@gmail.com>
2025-05-16 19:17:49 +0300

    updates path

--- app/main.py
+++ app/main.py
@@ -1,21 +1,37 @@
-from fastapi import FastAPI, HTTPException
+from fastapi import FastAPI, Query
+from fastapi.responses import FileResponse, JSONResponse
 from pytube import YouTube
+import os
+import uuid
+from fastapi.middleware.cors import CORSMiddleware
 
 app = FastAPI()
 
-@app.get("/download")
-async def download_video(url: str):
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],  
+    allow_credentials=True,
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
+DOWNLOAD_DIR = "downloads"
+
+os.makedirs(DOWNLOAD_DIR, exist_ok=True)
+
+@app.post("/download/")
+async def download_youtube_video(url: str = Query(..., description="The YouTube video URL")):
     try:
         yt = YouTube(url)
-
-        stream = yt.streams.get_highest_resolution()
-
-        stream.download()
-
-        return {"message": "Video downloaded successfully!"}
-    except KeyError:
-        raise HTTPException(status_code=400, detail="Error: Video is not available or cannot be downloaded")
-    except ValueError:
-        raise HTTPException(status_code=400, detail="Error: Invalid URL")
+        
+        stream = yt.streams.filter(progressive=True, file_extension="mp4").order_by("resolution").desc().first()
+        
+        if not stream:
+            return JSONResponse(status_code=404, content={"error": "No MP4 stream available for this video."})
+        
+        video_filename = f"{uuid.uuid4()}.mp4"
+        video_filepath = os.path.join(DOWNLOAD_DIR, video_filename)
+        stream.download(output_path=DOWNLOAD_DIR, filename=video_filename)
+        
+        return FileResponse(video_filepath, media_type="video/mp4", filename=f"{yt.title}.mp4")
     except Exception as e:
-        raise HTTPException(status_code=400, detail="Error downloading video: " + str(e))
+        return JSONResponse(status_code=500, content={"error": str(e)})
